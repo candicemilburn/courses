8f21d6ea48993943ede29d53b2a3f972f13c85fd
Candice Milburn <candicemilburn@hotmail.com>
2024-06-07 11:20:42 -0500

    saving progress like a good coder

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,32 @@
 "use strict"
 
 window.onload = () => {
- 
+    console.log("howdy partner");
+
+    populateTable();
    
-    
- 
 }
 
-console.log("howdy partner");
+async function populateTable(){
+
+    //get the courses from the API url
+    let courses = await getCourses();
+
+    console.log(courses);
+}
+
+async function getCourses(){
+    //make the API call all the courses
+    // the try says try and if the fetch await
+    // doesnt work it will fall into catch and deal the error
+    try{
+    let response = await fetch("http://localhost:8081/api/courses");
+    let courses = await response.json();
+
+    return courses;
+    }catch(error){
+        console.log(error)
+        throw new Error(error)
+    }
+}
+
